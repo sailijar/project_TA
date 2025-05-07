5b71df998879e08c26d2b680b34e052253dc0f4b
sailijar <sailijar88@gmail.com>
2025-05-07 14:13:46 +0700

    membuat halaman pelanggan

---
+++ database/migrations/2025_05_06_085504_create_pelanggans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pelanggans', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_pelanggan');
+            $table->string('no_handphone');
+            $table->text('alamat_pengantar');
+            $table->timestamps();
+            $table->softDeletes();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pelanggans');
+    }
+};
